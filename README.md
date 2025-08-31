80e74989820f09d6d86b6e73890151f6e9377746
Kaiyuan Han <hermit_han@163.com>
2025-09-01 02:52:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-quartet
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/aaranyue/quarTeT
+
+A telomere-to-telomere toolkit for gap-free genome assembly and centromeric repeat identification
---
+++ quartet.info.lisp
@@ -0,0 +1,16 @@
+(:name           "quartet")
+(:repo           "taffish-org/quartet")
+(:author         "KaiyuanHan")
+(:version        "1.2.5")
+(:intro          "A telomere-to-telomere toolkit for gap-free genome assembly and centromeric repeat identification")
+(:dependencies)
+(:functions      "<quartet> (quartet.py, ...)"
+                 "minimap2 [v2.29]"
+                 "unimap   [v0.1]"
+                 "mummer   [v4.0.1]"
+                 "trf      [v4.09.1]"
+                 "<cd-hit> [v4.8.1]"
+                 "<blast>  [v2.16.0]"
+                 "tidk     [v0.2.65]"
+                 "gnuplot  [v5.4]"
+                 "<r-base> (rideogram, ggplot2)")
